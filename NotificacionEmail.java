84b75ca6469645657aa998d8a3d230a59c1baeda
Daniel Celedon <celedondaniel21@gmail.com>
2026-05-04 19:49:54 -0300

    Delete NotificacionEmail.java

--- NotificacionEmail.java
+++
@@ -1,8 +0,0 @@
-//Principios de Responsabilidad única,Principio de segregación de interfaz
-public class NotificacionEmail implements INotificaciones {
-
-    @Override
-    public void enviarNotificacion(String destinatario, String mensaje) {
-        System.out.println("Enviando correo a " + destinatario + ": " + mensaje);
-    }
-}
