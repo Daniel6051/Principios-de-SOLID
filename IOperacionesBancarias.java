95c7bfcf82192b8ebc87280ed8b8d21c0202716e
Daniel Celedon <celedondaniel21@gmail.com>
2026-05-04 19:49:38 -0300

    Delete IOperacionesBancarias.java

--- IOperacionesBancarias.java
+++
@@ -1,5 +0,0 @@
-//Principios de Responsabilidad única,Principio de segregación de interfaz
-public interface IOperacionesBancarias {
-    void depositar(double monto);
-    void retirar(double monto);
-}
